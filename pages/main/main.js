c929c0fe5ebd3dd81b4fed1af758aba51bd620d5
maotian <mtian999@163.com>
2017-05-05 17:16:15 +0800

    init

---
+++ pages/main/main.js
@@ -0,0 +1,39 @@
+//logs.js
+var util = require('../../utils/util.js')
+Page({
+  data: {
+    array: ['街道', '便利店', '书店', '咖啡厅','餐厅'],
+    objectArray: [
+      {
+        id: 0,
+        name: '美国'
+      },
+      {
+        id: 1,
+        name: '中国'
+      },
+      {
+        id: 2,
+        name: '巴西'
+      },
+      {
+        id: 3,
+        name: '日本'
+      }
+    ],
+    index: 0
+  },
+  onLoad: function () {
+    this.setData({
+      logs: (wx.getStorageSync('logs') || []).map(function (log) {
+        return util.formatTime(new Date(log))
+      })
+    })
+  },
+  bindPickerChange: function (e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      index: e.detail.value
+    })
+  }
+})
